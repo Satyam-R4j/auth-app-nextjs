a5c2969eef138306348f393ca45fb901b933b362
Satyam-R4j <satyamraj418@gmail.com>
2025-09-03 16:27:01 +0530

    Creating login,profile,singup pages

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function profilePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="">Profile</h1>
+            <hr />
+            <p>Profile page</p>
+        </div>
+    );
+}
