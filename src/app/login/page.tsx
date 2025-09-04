47718bbd924489befa035bc735726fe9a4957040
Satyam-R4j <satyamraj418@gmail.com>
2025-09-04 17:56:51 +0530

    login and signup page completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,45 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-export default function loginPage() {
+export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
     });
+    const [buttonDisable, setButtonDisable] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-    const onLogin = async () => {};
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
+            router.push("/profile");
+        } catch (error: any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisable(false);
+        } else {
+            setButtonDisable(true);
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>{loading ? "Processing" : "Login"}</h1>
             <hr />
             <label htmlFor="email">email</label>
             <input
@@ -25,7 +50,7 @@ export default function loginPage() {
                 onChange={(e) => setUser({ ...user, email: e.target.value })}
                 placeholder="email"
             />
-            <label htmlFor="password">email</label>
+            <label htmlFor="password">Password</label>
             <input
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
                 id="password"
