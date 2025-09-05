7befb20a76c87d2322b802e203000b7b0c4c1c57
Satyam-R4j <satyamraj418@gmail.com>
2025-09-05 18:06:25 +0530

    Logout completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function profilePage() {
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            toast.success("Logout successful");
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+            router.push("/login ");
+        }
+    };
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="">Profile</h1>
             <hr />
             <p>Profile page</p>
+            <hr />
+            <button
+                onClick={logout}
+                className="bg-orange-500 hover:bg-orange-400 text-black font-bold py-2 px-4 rounded"
+            >
+                Logout
+            </button>
         </div>
     );
 }
